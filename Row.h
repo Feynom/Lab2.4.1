7abb1737471887d054a12585878d8a0e06484c2a
Feynom <53947611+Feynom@users.noreply.github.com>
2021-03-18 17:24:59 +0200

    Add files via upload

---
+++ Row.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Row
+{
+public:
+	double* v;
+
+	Row();
+	Row(int);
+	double& operator [] (int);
+};
+
---
+++ UnitTest2.4.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab2.4/Lab2.4/Matrix.h"
+#include "../Lab2.4/Lab2.4/Matrix.cpp"
+#include "../Lab2.4/Lab2.4/Row.h"
+#include "../Lab2.4/Lab2.4/Row.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest24
+{
+	TEST_CLASS(UnitTest24)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Matrix A(2), B(2);
+			Assert::AreEqual(A == B, true);
+		}
+	};
+}
